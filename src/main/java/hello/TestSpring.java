4ab424b41b245b52b0803daa382f3d75d7935545
Gurtovenko Roman <gurtovenko@ibcon.ru>
2020-04-18 20:27:40 +0300

    Alishev - lessons 7 Alishev - lessons 6 Alishev - lessons 5 Alishev - lessons 4

--- src/main/java/hello/TestSpring.java
+++ src/main/java/hello/TestSpring.java
@@ -7,13 +7,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Music music = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playSong();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comprassion = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comprassion);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
     }
 }
